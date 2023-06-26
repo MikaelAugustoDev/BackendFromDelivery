cdae1e293a78366660bcb540d11dcbf67b36193c
Mikael Augusto <mikaelaugustodev@gmail.com>
2023-06-26 11:36:36 -0300

    configurando e conectando prisma ao banco de dados

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,15 @@ import express from "express";
 dotenv.config();
 const app = express();
 
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+app.get("/user", async (req, res) => {
+    const users = await prisma.usuario.findMany();
+    res.json(users);
+});
+
 const PortEnv = process.env.PORT;
 
 app.listen(PortEnv, () => console.log("Servidor Rodando com Sucesso!"));
